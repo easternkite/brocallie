e9ff8364c2f426f57d60afc9a82a1c8d7aa3f36c
Lee Dong-Yeon <blucky4511@gmail.com>
2024-07-26 01:27:32 +0900

    add insert query in callies table

--- shared/core/room/src/commonMain/kotlin/com.blucky8649.room/dao/CallieDao.kt
+++ shared/core/room/src/commonMain/kotlin/com.blucky8649.room/dao/CallieDao.kt
@@ -1,6 +1,7 @@
 package com.blucky8649.room.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
 import androidx.room.Query
 import com.blucky8649.room.model.CallieEntity
 import kotlinx.coroutines.flow.Flow
@@ -9,4 +10,7 @@ import kotlinx.coroutines.flow.Flow
 interface CallieDao {
     @Query("SELECT * FROM callies")
     fun getCallieList() : Flow<List<CallieEntity>>
+
+    @Insert
+    fun insertCallie(callieEntity: CallieEntity)
 }
