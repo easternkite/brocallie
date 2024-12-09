15a4abb1001f8fb8a831ddccb72f08f44c94660e
Lee Dong-Yeon <blucky4511@gmail.com>
2024-12-09 22:35:40 +0900

    remove unused module

--- shared/core/tts/build.gradle.kts
+++
@@ -1,4 +0,0 @@
-plugins {
-    alias(libs.plugins.brocallie.kotlin.multiplatform.shared)
-    alias(libs.plugins.brocallie.compose.multiplatform.shared)
-}
--- shared/core/tts/src/commonMain/kotlin/com/blucky8649/tts/Speechable.kt
+++
@@ -1,6 +0,0 @@
-package com.blucky8649.tts
-
-interface Speechable {
-    val tts: TextToSpeech
-    fun speak(text: String, language: String)
-}
