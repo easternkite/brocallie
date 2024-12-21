0f40fa92dafa532ecef5ed0650454f728c1faac2
Lee Dong-Yeon <easternkite7@gmail.com>
2024-12-22 02:02:31 +0900

    Add Push Image Loader in Android target (#72)

--- androidApp/src/main/java/com/blucky8649/brocallie/push/BcFcmService.kt
+++ androidApp/src/main/java/com/blucky8649/brocallie/push/BcFcmService.kt
@@ -3,18 +3,28 @@ package com.blucky8649.brocallie.push
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Intent
+import android.graphics.Bitmap
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import coil3.ImageLoader
+import coil3.request.ImageRequest
+import coil3.request.allowHardware
+import coil3.toBitmap
 import com.blucky8649.brocallie.R
 import com.blucky8649.brocallie.android.MainActivity
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class BcFcmService : FirebaseMessagingService() {
     companion object {
         const val TAG = "BcFcmService"
     }
+
     override fun onNewToken(token: String) {
         super.onNewToken(token)
         Log.d(TAG, "onNewToken: $token")
@@ -28,20 +38,27 @@ class BcFcmService : FirebaseMessagingService() {
             flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         }.let { PendingIntent.getActivity(this, 0, it, PendingIntent.FLAG_IMMUTABLE) }
 
-        val pushMessage = message.toPushMessage()
-        val messageBuilder = NotificationCompat
-            .Builder(this, pushMessage.channelId)
-            .setSmallIcon(R.drawable.ic_push_brocallie)
-            .setColor(getColor(R.color.PUSH_GREEN))
-            .setContentTitle(pushMessage.title)
-            .setContentText(pushMessage.body)
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setContentIntent(pendingIntent)
-            .setAutoCancel(true)
+        CoroutineScope(Dispatchers.IO).launch {
+            val pushMessage = message.toPushMessage()
+            val iconBitmap = getPushImage(pushMessage.imageUrl ?: "")
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-        NotificationManagerCompat.from(this).apply {
-            notificationManager.notify(System.currentTimeMillis().toInt(), messageBuilder.build())
+            val messageBuilder = NotificationCompat
+                .Builder(this@BcFcmService, pushMessage.channelId)
+                .setSmallIcon(R.drawable.ic_push_brocallie)
+                .setColor(getColor(R.color.PUSH_GREEN))
+                .setLargeIcon(iconBitmap)
+                .setContentTitle(pushMessage.title)
+                .setContentText(pushMessage.body)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+
+            withContext(Dispatchers.Main) {
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+                NotificationManagerCompat.from(this@BcFcmService).apply {
+                    notificationManager.notify(System.currentTimeMillis().toInt(), messageBuilder.build())
+                }
+            }
         }
     }
 
@@ -49,4 +66,16 @@ class BcFcmService : FirebaseMessagingService() {
         super.onDeletedMessages()
         Log.d(TAG, "onDeletedMessages")
     }
+}
+
+suspend fun BcFcmService.getPushImage(url: String): Bitmap? {
+    val loader = ImageLoader(this)
+    val request = ImageRequest.Builder(this)
+        .data(url)
+        .allowHardware(false)
+        .build()
+
+    return loader.execute(request)
+        .image
+        ?.toBitmap()
 }
