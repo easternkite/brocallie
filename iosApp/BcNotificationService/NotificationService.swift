868723ebf2f430f0498d0f56d52eee603b4426a5
Lee Dong-Yeon <easternkite7@gmail.com>
2024-12-24 22:41:03 +0900

    Add Notification Service Extension in iOS (#73)

---
+++ iosApp/BcNotificationService/NotificationService.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationService.swift
+//  BcNotificationService
+//
+//  Created by 이동연 on 12/24/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import UserNotifications
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        guard let content = bestAttemptContent else { return }
+        content.title = "\(content.title)"
+        
+        contentHandler(content)
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
